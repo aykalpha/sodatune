60138a0134dcb94bdcdd93cbff4f19b5779227ae
aykalpha <ayaka@gmail.com>
2025-10-13 12:09:39 +0900

    調整

--- react/src/components/LineChart.tsx
+++ react/src/components/LineChart.tsx
@@ -12,13 +12,14 @@ export default function LineChartCard({ soilMoisture }: LineChartCardProps) {
     const date = new Date(measured_at);
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
-    return `${hours}:${minutes}`;
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    return `${hours}:${minutes}:${seconds}`;
   };
-
+  
   return (
     <Card>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={soilMoisture.slice(-12)} margin={{ top: 10, right: 25, left: 10, bottom: 10 }}>
+        <LineChart data={soilMoisture.slice(0,10).reverse()} margin={{ top: 10, right: 30, left: 10, bottom: 10 }}>
           <CartesianGrid stroke="rgba(255,255,255,0.2)" />
           <XAxis
             dataKey="measured_at"
