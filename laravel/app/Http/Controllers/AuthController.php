e926a5737fd7f8c918fad5b615a70c6c596ddbf6
aykalpha <ayaka@gmail.com>
2025-11-02 09:14:24 +0900

    マイグレーション整理

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -9,30 +9,26 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     // Googleログインページへリダイレクト
-public function redirectToGoogle()
-{
-    return Socialite::driver('google')
-        ->scopes(['openid', 'profile', 'email'])
-        ->redirect();
-}
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')
+            ->scopes(['openid', 'profile', 'email'])
+            ->redirect();
+    }
 
     // Google認証後のコールバック
     public function handleGoogleCallback()
     {
         $googleUser = Socialite::driver('google')->stateless()->user();
-    
         $user = User::updateOrCreate(
-            ['email' => $googleUser->getEmail()],
+            ['id' => $googleUser->getId()],
             [
                 'name' => $googleUser->getName(),
-                'google_id' => $googleUser->getId(),
-                'avatar' => $googleUser->getAvatar(),
+                'email' => $googleUser->getEmail(),
+                'avatar' => $googleUser->getAvatar()
             ]
         );
-
         Auth::login($user);
-
-        // 認証後、Reactのトップページへリダイレクト
         return redirect('http://localhost:3000/');
     }
 }
